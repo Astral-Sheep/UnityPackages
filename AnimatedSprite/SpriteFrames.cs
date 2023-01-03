40c651a6f6d909b05579998db8d52ddc7dffa382
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2023-01-03 19:17:14 +0100

    feat: rename class SpriteFrames as AnimationFrames, and add all methods in classes AnimatedSprite and AnimationFrames

--- AnimatedSprite/SpriteFrames.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = nameof(SpriteFrames), menuName = nameof(SpriteFrames))]
-public class SpriteFrames : ScriptableObject
-{
-	[SerializeField] protected string animationName = "default";
-	[SerializeField] protected List<Sprite> frames = new List<Sprite>();
-}
